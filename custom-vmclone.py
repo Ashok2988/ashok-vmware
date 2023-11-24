71f18a2f004255a9c2ae7d91fd5940e5d33fd562
Ashok <ashokkumaran.b@gmail.com>
2023-11-24 13:22:30 +0530

    custom-vmclone.py v2

--- custom-vmclone.py
+++ custom-vmclone.py
@@ -32,7 +32,7 @@ import argparse
 def subnet(cidr):
     li=[str(ip) for ip in ipaddress.IPv4Network(cidr)]
     mask=str(ipaddress.IPv4Network(cidr).netmask)
-    li.pop() 
+    li.pop()
     return li,mask
 def main():
 
@@ -62,7 +62,7 @@ def main():
     parser.add_argument('--gw',
                         help='gw',
                         required=True, default=False)
-     
+
     args=parser.parse_args()
     si = SmartConnectNoSSL(host=args.vcip, user=args.vcuser, pwd=args.vcpwd)
     with requests.session() as session:
@@ -79,7 +79,10 @@ def main():
              if vm.name == args.tempname:
                 template = vm
                 break
-
+        for net in root_folder.childEntity[0].network:
+               if net.name == args.network:
+                   nn=net
+                   break
         if template is None:
            raise Exception(f"Template '{template_name}' not found.")
         cluster = 'cluster2'
@@ -90,28 +93,29 @@ def main():
 
         if cluster is None:
            raise Exception(f"Cluster '{cluster_name}' not found.")
-        sub,mask=subnet(args.cidr)    
+        sub,mask=subnet(args.cidr)
         datastores = content.viewManager.CreateContainerView(content.rootFolder, [vim.Datastore], True).view
         for ds in datastores:
               if ds.name=='vsanDatastore':
                  datastore=ds
-                 break      
+                 break
         for i in range(int(args.vms)):
            deploy_spec = vim.vm.ConfigSpec()
            deploy_spec.numCPUs = 1
            deploy_spec.memoryMB = 1024
            network_spec = vim.vm.device.VirtualDeviceSpec()
+           #network_spec.operation=vim.vm.device.VirtualDeviceSpec.Operation.edit
            network_spec.device = vim.vm.device.VirtualVmxnet3()
            network_spec.device.deviceInfo = vim.Description()
            network_spec.device.deviceInfo.label = 'Network Adapter 1'
-           network_spec.device.deviceInfo.summary = 'DPortGroup-2'
+#           network_spec.device.deviceInfo.summary = '
 
            adapter_map = vim.vm.customization.AdapterMapping()
            adapter_map.adapter = vim.vm.customization.IPSettings()
            adapter_map.adapter.ip = vim.vm.customization.FixedIp()
            ipa=sub.pop()
            hname=ipa.split('.')
-           
+
            adapter_map.adapter.ip.ipAddress = ipa
            adapter_map.adapter.subnetMask = mask
            adapter_map.adapter.gateway = args.gw
@@ -129,30 +133,40 @@ def main():
            customization_spec.identity=ident
            customization_spec.globalIPSettings=global_ip_settings
            network_spec.device.backing = vim.vm.device.VirtualEthernetCard.NetworkBackingInfo()
-           network_spec.device.backing.network = template.network[0]
-           network_spec.device.backing.deviceName = template.network[0].name
+           network_spec.device.backing.network = nn
+           network_spec.device.backing.deviceName = nn.name
            network_spec.device.backing.useAutoDetect = True
            network_spec.device.connectable = vim.vm.device.VirtualDevice.ConnectInfo()
            network_spec.device.connectable.startConnected = True
            network_spec.device.connectable.allowGuestControl = True
+
 
            deploy_spec.deviceChange = [network_spec]
 
            relospec = vim.vm.RelocateSpec()
            relospec.pool = cluster.resourcePool
            relospec.datastore=datastore
-           task = template.Clone(name='-'.join(hname),folder=template.parent,  spec=vim.vm.CloneSpec(config=deploy_spec, location=relospec, customization=customization_spec,powerOn=True ))
+           task = template.Clone(name='-'.join(hname),folder=template.parent,  spec=vim.vm.CloneSpec(config=deploy_spec, location=relospec, customization=customization_spec ))
+
            while task.info.state not in [vim.TaskInfo.State.success, vim.TaskInfo.State.error]:
               pass
 
            if task.info.state == vim.TaskInfo.State.error:
               raise Exception(f"Deploy task failed: {task.info.error}")
-           new=task.info.result 
-           token=get_session()
-           token=token.strip('"')
-           time.sleep(2)
-           response=requests.post('https://ashok-vcf-compute-vc.cptroot.com/api/vcenter/vm/%s/hardware/ethernet/4000?action=connect' %new._moId,verify=False, headers={"vmware-api-session-id": "%s" %token})
-           print(response.text)
+           o=task.info.result._moId
+           nic=(client.vcenter.vm.hardware.Ethernet.list(vm=o))[0]
+           nic_update_spec = Ethernet.UpdateSpec(
+                                        backing=Ethernet.BackingSpec(
+                                        type=Ethernet.BackingType.DISTRIBUTED_PORTGROUP,
+                                        network=nn._moId))
+           client.vcenter.vm.hardware.Ethernet.update(o,'4000',nic_update_spec)
+           task.info.result.PowerOnVM_Task()
+#           new=task.info.result
+#           token=get_session()
+#           token=token.strip#('"')
+#           time.sleep(2)
+#           response=requests.post('https://ashok-vcf-compute-vc.cptroot.com/api/vcenter/vm/%s/hardware/ethernet/4000?action=connect' %new._moId,verify=False, headers={"vmware-api-session-id": "%s" %token})
+#           print(response.text)
         Disconnect(si)
 
 
